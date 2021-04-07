1a419871d6baadd6d37c1a339542b6b43c576f11
perpetualgrimace <hello@jamesferrell.me>
2021-04-07 19:09:33 -0400

    adds base styles

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,12 +1,14 @@
   <?= js('assets/builds/bundle.js') ?>
 
-	  <ul><? foreach ($pages->not('home')->not('error') as $item): ?>
-	  	<li>
-	  		<a href="<?= $item->slug() ?>">
-	  			<?= $item->title()->html() ?>
-	  		</a>
-	  	</li>
-	  <? endforeach ?></ul>
+  	<nav>
+		  <ul><? foreach ($pages->not('home')->not('error') as $item): ?>
+		  	<li>
+		  		<a href="<?= $item->slug() ?>">
+		  			<?= $item->title()->html() ?>
+		  		</a>
+		  	</li>
+		  <? endforeach ?></ul>
+	  </nav>
 
   </body>
 </html>
--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -4,16 +4,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;600&display=swap" rel="stylesheet">
+
   <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
   <?= liveCSS('assets/builds/bundle.css') ?>
 </head>
 
-<ul><? foreach ($site->children()->listed() as $item): ?>
-	<li>
-		<a href="<?= $item->slug() ?>">
-			<?= $item->title()->html() ?>
-		</a>
-	</li>
-<? endforeach ?></ul>
+<nav>
+	<a class="logo" href="<?= $site->url() ?>">
+		<?= $site->title() ?>
+	</a>
+	<ul><? foreach ($site->children()->listed() as $item): ?>
+		<li>
+			<a href="<?= $item->slug() ?>">
+				<?= $item->title()->html() ?>
+			</a>
+		</li>
+	<? endforeach ?></ul>
+</nav>
 
 <body>
