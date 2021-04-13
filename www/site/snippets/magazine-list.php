f30142fd46ed76c0ddf6e5e7cafda25cc62f67d2
perpetualgrimace <hello@jamesferrell.me>
2021-04-13 18:54:51 -0400

    gets magazine-list filtering working

--- www/site/snippets/magazine-list.php
+++ www/site/snippets/magazine-list.php
@@ -29,7 +29,7 @@
 	<div class="magazine-outer">
 		<ul class="magazine-list">
 			<? foreach($allMagazines as $magazine): ?>
-				<li class="magazine-item" data-category="<?= matchCategories($categories, $magazine) ?>">
+				<li class="magazine-item is-hidden" data-category="<?= matchCategories($categories, $magazine) ?>">
 					<img class="magazine-img" src="<?= $magazine->url() ?>" alt="<?= $magazine->alt() ?>" draggable="false" loading="lazy" width="225" height="<?= $magazine->height() / 2 ?>">
 				</li>
 			<? endforeach ?>
@@ -37,3 +37,33 @@
 		
 	</div>
 <? endif ?>
+
+
+<script>
+
+	function filterMags(category) {
+		var allMagazines = document.querySelectorAll('.magazine-item');
+		Array.from(allMagazines).map(li => li.className = 'magazine-item is-hidden')
+
+		var matches = document.querySelectorAll(`.magazine-item[data-category*=${category}]`);
+		Array.from(matches).map(li => li.className = 'magazine-item');
+	}
+
+	// on load
+	document.addEventListener('DOMContentLoaded', (event) => {
+    var category = 'all';
+
+    if (location.hash) {
+    	category = location.hash.replace('#', '');
+    }
+
+    filterMags(category);
+	});
+
+	// on hash change
+	window.addEventListener('hashchange', function() {
+		category = location.hash.replace('#', '');
+
+		filterMags(category);
+	});
+</script>
