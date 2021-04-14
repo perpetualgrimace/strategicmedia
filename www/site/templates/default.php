f896a0040a64d7f5902c4bffedc1b722c59fd9c5
perpetualgrimace <hello@jamesferrell.me>
2021-04-14 12:21:07 -0400

    improves default template

--- www/site/templates/default.php
+++ www/site/templates/default.php
@@ -4,7 +4,11 @@
 	<? snippet('hero') ?>
 
 	<? snippet('main-open') ?>
-		
+		<div class="g-container u-margin-top-lg u-margin-bottom-lg">
+			<div class="g-col">
+				<?= $page->text()->kt() ?>
+			</div>
+		</div>
 	<? snippet('main-close') ?>
 <? snippet('body-close') ?>
 
