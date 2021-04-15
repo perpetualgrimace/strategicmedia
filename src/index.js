f23f3dd36a91c1525110484dd4eaf2f1dd651acb
perpetualgrimace <hello@jamesferrell.me>
2021-04-15 19:01:23 -0400

    makes nav & hero fancy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('It\'s working!')
+window.onload = () => {
+	function throttle(func, timeFrame) {
+		var lastTime = 0;
+		return function() {
+			var now = new Date();
+			if (now - lastTime >= timeFrame) {
+				func();
+				lastTime = now;
+			}
+		}
+	}
+
+	const nav = document.querySelector('.header-nav');
+	const hero = document.querySelector('.hero');
+	const heroText = document.querySelector('.hero-inner');
+
+	function handleScroll(e) {
+		const pxUntilHero = window.pageYOffset - hero.offsetHeight;
+
+		if (pxUntilHero >= -nav.offsetHeight) {
+			nav.classList.add('is-fixed');
+		}
+
+		else {
+			nav.classList.remove('is-fixed');
+		}
+
+	  var fade = (((hero.offsetHeight * 0.75) - window.pageYOffset) / (hero.offsetHeight * 0.75));
+	  var scale = (((hero.offsetHeight * 4) - window.pageYOffset) / (hero.offsetHeight * 4));
+
+	  heroText.style.opacity = fade;
+	  heroText.style.transform = `translateY(${window.pageYOffset / 2}px) scale(${scale})`;
+	}
+
+	// ready
+	document.querySelector('html').classList = '';
+
+	// scrolling
+	if (window.innerWidth >= 756) {
+		document.querySelector('html').classList = 'is-big-enough';
+
+		document.addEventListener("scroll", function(e) {
+			throttle(handleScroll(e), 100);
+		});
+	}
+}
