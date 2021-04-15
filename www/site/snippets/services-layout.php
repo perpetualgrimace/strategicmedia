b7a7cc7bd7945817c2b432d0864f4317c6a6c64e
perpetualgrimace <hello@jamesferrell.me>
2021-04-15 13:55:38 -0400

    adds & styles services list

---
+++ www/site/snippets/services-layout.php
@@ -0,0 +1,37 @@
+<div class="g-container u-margin-top-lg u-margin-bottom-lg">
+	<div class="g-col g-12">
+		<?= $page->text()->kt() ?>
+	</div>
+</div>
+
+
+<!-- real heading -->
+<? if($page->servicesHeading() != ''): ?>
+	<h2 class="u-visually-hidden"><?= $page->servicesHeading() ?></h2>
+<? endif ?>
+
+<ul class="services service-list g-container block is-behind u-center">
+
+	<!-- "heading" -->
+	<? if($page->servicesHeading() != ''): ?>
+		<li class="services-heading heading u-font-xl g-col u-margin-bottom-md" aria-hidden role="presentation">
+			<?= $page->servicesHeading() ?>
+		</li>
+	<? endif ?>
+
+	<? if($page->services()->toStructure() != ''): ?>
+		<? foreach($page->services()->toStructure() as $i => $service): ?>
+			<li class="service-item g-col g-<? e($i <= 2, '4', '6') ?>">
+				<span class="service-icon u-margin-bottom-md">
+					<? snippet('icons/' . $service->icon() . '.svg') ?>
+				</span>
+
+				<span class="service-title heading u-font-sm">
+					<?= $service->title() ?>
+				</span>
+
+				<? snippet('divider') ?>
+			</li>
+		<? endforeach ?>
+	<? endif ?>
+</ul>
---
+++ www/site/templates/services.php
@@ -0,0 +1,11 @@
+<? snippet('header') ?>
+
+<? snippet('body-open') ?>
+	<? snippet('hero') ?>
+
+	<? snippet('main-open') ?>
+		<? snippet('services-layout') ?>
+	<? snippet('main-close') ?>
+<? snippet('body-close') ?>
+
+<? snippet('footer') ?>
