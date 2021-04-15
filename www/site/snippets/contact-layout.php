d29f498934f0dff02feba6ca533dffdcffdc6731
perpetualgrimace <hello@jamesferrell.me>
2021-04-15 15:20:36 -0400

    adds & styles contact page content

---
+++ www/site/snippets/contact-layout.php
@@ -0,0 +1,8 @@
+<div class="contact g-container u-margin-top-lg content">
+	<div class="g-col g-6 u-margin-bottom-lg">
+		<?= $page->col1()->kt() ?>
+	</div>
+	<div class="g-col g-6 u-margin-bottom-lg">
+		<?= $page->col2()->kt() ?>
+	</div>
+</div>
---
+++ www/site/templates/contact.php
@@ -0,0 +1,11 @@
+<? snippet('header') ?>
+
+<? snippet('body-open') ?>
+	<? snippet('hero') ?>
+
+	<? snippet('main-open') ?>
+		<? snippet('contact-layout') ?>
+	<? snippet('main-close') ?>
+<? snippet('body-close') ?>
+
+<? snippet('footer') ?>
